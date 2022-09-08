e3ea4b85867b8f998878dc233b20b6a41ad714db
Yurii Rohoza <unixexp@gmail.com>
2022-09-08 13:27:52 +0300

    add menu js media request

--- src/index.js
+++ src/index.js
@@ -5,9 +5,6 @@ import "./assets/scss/header/header.scss";
 import "./assets/scss/main/main.scss";
 import "./assets/scss/footer/footer.scss";
 import "./components/slider/slider.scss";
-import "./assets/scss/media.scss";
-
-import { MEDIA_MAX_WIDTH_688, MEDIA_MAX_WIDTH_996 } from "./media.js";
 
 import Slider from "./components/slider/slider.js";
 
@@ -46,8 +43,9 @@ newsSlider.setCallback(watchSliderState);
 
 // MainMenu block
 const mainMenuButton = document.querySelector(".menu-btn");
+const mainMenu = document.querySelector(".menu");
+
 const displayMenu = () => {
-	const mainMenu = document.querySelector(".menu");
 	const minimized = mainMenu.classList.contains("menu--minimized");
 	if (!minimized) {
 		mainMenu.classList.add("menu--minimized");
@@ -59,13 +57,16 @@ const displayMenu = () => {
 };
 mainMenuButton.addEventListener("click", displayMenu);
 
-if (MEDIA_MAX_WIDTH_688) {
-	displayMenu();
+window.addEventListener("load", () => {
+	if (window.matchMedia('(max-width: 996px)').matches) {
+		mainMenu.classList.add("menu--minimized");
+		mainMenu.classList.add("menu--hidden");
+	}
+});
 
-	const headerRightBlock = document.querySelector(".header__right-block");
-	const headerPhones = headerRightBlock.querySelector(".header__phones");
-	headerRightBlock.insertBefore(mainMenuButton, headerPhones);
-} else if (MEDIA_MAX_WIDTH_996) {
-	displayMenu();
-}
-
+window.addEventListener("resize", () => {
+	if (window.matchMedia('(max-width: 966px)').matches) {
+		mainMenu.classList.add("menu--minimized");
+		setTimeout(() => mainMenu.classList.add("menu--hidden"), 300);
+	}
+});
--- src/media.js
+++
@@ -1,2 +0,0 @@
-export let MEDIA_MAX_WIDTH_688 = window.matchMedia('(max-width: 688px)').matches;
-export let MEDIA_MAX_WIDTH_996 = window.matchMedia('(max-width: 996px)').matches;
